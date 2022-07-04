9d5ab8ea4d694c29002c71078ebfd44f1490cd19
David Marques <david.marques@moia.io>
2022-07-04 19:43:31 +0200

    Modified config

--- init.el
+++ init.el
@@ -21,4 +21,16 @@
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-mode rust-mode zenburn-theme which-key use-package undo-tree telephone-line smartparens sly rainbow-delimiters projectile magit jupyter hydra flycheck evil elpy dashboard counsel company-jedi beacon auctex amx)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

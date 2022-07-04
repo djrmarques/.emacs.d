17ac19f6513bb2742e156897de2b596968d1d16a
David Marques <david.marques@moia.io>
2022-07-04 19:40:43 +0200

    Fist commit

---
+++ init.el
@@ -0,0 +1,24 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(setq package-enable-at-startup nil)
+
+(unless (package-installed-p 'use-package) (package-refresh-contents) (package-install 'use-package))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+(setq org-config-file "~/.emacs.d/config.org")
+(org-babel-load-file (expand-file-name org-config-file))
+
+(setq ring-bell-function 'ignore)
+
+(global-prettify-symbols-mode t) 
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)

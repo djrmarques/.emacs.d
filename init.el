af15336dba5d28c9929f76a6af7caf70fc9a92e3
David Marques <david.marques@moia.io>
2022-07-12 20:50:14 +0200

    COnfig is not entirely on config.org

--- init.el
+++ init.el
@@ -9,28 +9,6 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
 (setq org-config-file "~/.emacs.d/config.org")
 (org-babel-load-file (expand-file-name org-config-file))
 
-(setq ring-bell-function 'ignore)
-
-(global-prettify-symbols-mode t) 
-
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-mode rust-mode zenburn-theme which-key use-package undo-tree telephone-line smartparens sly rainbow-delimiters projectile magit jupyter hydra flycheck evil elpy dashboard counsel company-jedi beacon auctex amx)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
